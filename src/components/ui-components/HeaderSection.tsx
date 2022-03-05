f5b09886bc947f8bb62ff2a2d0397d2e28bd039d
saidoud <develop.softoud@gmail.com>
2022-03-05 17:04:03 +0100

    header section

--- src/components/ui-components/HeaderSection.tsx
+++ src/components/ui-components/HeaderSection.tsx
@@ -8,20 +8,12 @@ interface HeaderSectionProps {
 
 function HeaderSection({ title }: HeaderSectionProps) {
     return (
-        <Container maxWidth="xs" sx={{ textAlign: 'center', my: 4 }}>
-            <Typography component="p" variant="h4">
-                <GradientText>{title}</GradientText>
-            </Typography>
-            <Box
-                sx={(theme) => ({
-                    display: 'block',
-                    height: 3,
-                    borderRadius: 6,
-                    width: 50,
-                    mx: 'auto',
-                    backgroundColor: theme.palette.success.light
-                })}
-            />
+        <Container>
+            <Divider textAlign="left" sx={{ width: { xs: '70%', md: '40%' } }}>
+                <Typography component="p" variant="h5" align="center" color="primary">
+                    {title}
+                </Typography>
+            </Divider>
         </Container>
     );
 }
