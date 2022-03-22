608da802dfe5a481699fe5f23201f0ad2ab807ce
saidoud <develop.softoud@gmail.com>
2022-03-22 15:30:54 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,12 +17,12 @@ const Home: NextPage = () => {
                 <title>Create Next App</title>
                 <meta name="description" content="Full Stack Developer" />
                 <link rel="icon" href="/favicon.ico" />
-                {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
                     rel="stylesheet"
-                /> */}
+                />
             </Head>
             <Header />
             <main>
